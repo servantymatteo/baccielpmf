cabfb1290a5c814d74d8b8337148d3ef64ae8c85
Servanty Matteo <servantymatteo@gmail.com>
2024-01-18 10:22:28 +0100

    ajout 2 3 truc et sm

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,10 @@ import { Footer } from "@/ui/components/navigation/footer";
 export default function Home() {
   return (
     <>
-      <Seo title="BAC CIEL PMF" description="DESCRIPTION" />
+      <Seo
+        title="BAC CIEL PIERRE MENDES FRANCE"
+        description="Découvrez l'excellence éducative au lycée BAC CIEL PIERRE MENDES FRANCE. Notre filière offre une formation complète, un entraînement intensif et l'accès à des ressources innovantes. Parcourez virtuellement notre campus pour une immersion totale. Choisissez l'avenir avec BAC CIEL : l'éducation redéfinie, l'excellence assurée."
+      />
       <Navigation />
       <Footer />
     </>
